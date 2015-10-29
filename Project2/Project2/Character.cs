366e2d5d7a61a5cdac715426e231b06684664e2e
Taylor Bacon <tijikiotec@gmail.com>
2015-10-29 12:56:37 -0400

    Added the character class as a test to the commit and push.

---
+++ Project2/Project2/Character.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project2
+{
+    class Character
+    {
+        //Shared Stats
+        public int Health { get; set; }
+        public int Stamina { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int Magic { get; set; }
+        public int Experience { get; set; }
+
+        //Status Aligments
+        public bool Poisoned { get; set; }
+        public bool Burned { get; set; }
+        public bool Stun { get; set; }
+
+    }
+}
